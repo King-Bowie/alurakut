c6d971968d142e89d4f370a2640f551067550363
King-Bowie <reinaldinho.2008@gmail.com>
2021-07-15 02:23:41 -0300

    main

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,29 @@ function ProfileSideBar(propriedades) {
   )
 }
 
+function ProfileRelationsBox(propriedades){
+  return(
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        {propriedades.title} ({propriedades.items.length})
+      </h2>
+      <ul>
+          {/* {{comunidades.map((itemAtual) => {
+            return (
+              <li key={itemAtual.id}>
+                <a href={`/users/${itemAtual.title}`}>
+                    {<img src={itemAtual.image} />}
+                    <span>{itemAtual.title}</span>
+                </a>
+              </li>
+            )
+          })} } */} 
+        </ul>
+  </ProfileRelationsBoxWrapper>
+  )
+}
+
+
 export default function Home() {
   
   const usuarioAleatorio = 'King-Bowie';
@@ -48,6 +71,17 @@ export default function Home() {
   'marcobrunodev',
   'felipefialho'
 ]
+
+const [seguidores, setSeguidores] = React.useState([]);
+React.useEffect(function(){
+  fetch('https://api.github.com/users/King-Bowie/following')
+  .then(function (respostaDoServidor){
+    return respostaDoServidor.json();
+  })
+  .then(function(respostaCompleta){
+    setSeguidores(respostaCompleta);
+  })
+}, [])
 
   return( 
     <>
@@ -95,6 +129,7 @@ export default function Home() {
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBox title="Seguidores" items={seguidores}/>
           <ProfileRelationsBoxWrapper >
             <h2 className="smallTitle">
             <b>Pessoas da comunidade</b> ({pessoasFavoritas.length})
@@ -127,7 +162,6 @@ export default function Home() {
               )
             })} 
             </ul>
-
         </ProfileRelationsBoxWrapper>
           </Box>
         </div>
